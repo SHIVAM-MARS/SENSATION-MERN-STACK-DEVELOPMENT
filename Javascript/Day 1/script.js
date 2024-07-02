cfdb2389b45bfd73fd6d0a5a2698eabfc58be1e1
Shivam Sharma <shivamsharma965874@gmail.com>
2024-07-02 13:22:15 +0530

    JS class ends by this

--- Javascript/Day 1/script.js
+++ Javascript/Day 1/script.js
@@ -10,3 +10,20 @@ a = b;
 b = temp;
 
 console.log(`After swap: a = ${a}, b = ${b}`);
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+
+for(let i = 1; i<=10;i++){
+    console.log(`2 x ${i} = ${i*2}`)
+}
+
+prompt("Enter a Number:");
