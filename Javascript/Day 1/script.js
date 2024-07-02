db2365bbf6ced18cad5fbbcb27276a99cf4535e6
Shivam Sharma <shivamsharma965874@gmail.com>
2024-07-02 12:03:08 +0530

    learning javascript day 1 commitment

---
+++ Javascript/Day 1/script.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+let a = 5;
+let b = 10;
+
+console.log(`Before swap: a = ${a}, b = ${b}`);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`After swap: a = ${a}, b = ${b}`);
