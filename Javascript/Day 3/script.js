ef9e1c010dc1160f1fbf437fde48298a67a09e1c
Shivam Sharma <shivamsharma965874@gmail.com>
2024-07-04 13:18:56 +0530

    Test is on process

---
+++ Javascript/Day 3/script.js
@@ -0,0 +1,12 @@
+let name=["Aman","karan","rajat","abc","xyz"]
+
+// name.pop();// pop up the xyz
+// name.pop();// pop up the abc
+
+for(let i =1;i<5;i++){
+    name.pop()
+}
+
+// Push
+name.push("hello") // pushed hello to name array
+console.log(name.pop());
